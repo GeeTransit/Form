513aff161a58b599bf451a2e12825a9741ff1197
George Zhang <geetransit@gmail.com>
2020-11-19 17:18:17 -0500

    Use single letter codes
    w = text, m = multiple choice, c = checkboxes, d = date, t = time

--- form.py
+++ form.py
@@ -12,13 +12,11 @@ def format_time(entry, response):
 
 # General formatting function (uses a `type` argument)
 FORMATS = {
-    "Short Answer": format_normal,
-    "Paragraph": format_normal,
-    "Multiple Choice": format_sentinel,
-    "Checkboxes": format_normal,
-    "Dropdown": format_sentinel,
-    "Date": format_date,
-    "Time": format_time,
+    "w": format_normal,
+    "m": format_sentinel,
+    "c": format_normal,
+    "d": format_date,
+    "t": format_time,
 }
 def format_response(entry, type, response, *, required=True):
     if required and not response:
@@ -27,13 +25,13 @@ def format_response(entry, type, response, *, required=True):
 
 # Taken from https://docs.google.com/forms/d/e/1FAIpQLSfWiBiihYkMJcZEAOE3POOKXDv6p4Ox4rX_ZRsQwu77aql8kQ/viewform
 ENTRIES = {
-    2126808200: ["Short Answer", "Short Answer"],
-    647036320: ["Paragraph", "Paragraph"],
-    363426485: ["Multiple Choice", "Multiple Choice", "Option 1", "Option 2"],
-    1142411773: ["Checkboxes", "Checkboxes", "Option 1", "Option 2"],
-    2116902388: ["Dropdown", "Dropdown", "Option 1", "Option 2"],
-    465882654: ["Date", "Date"],
-    1049988990: ["Time", "Time"],
+    2126808200: ["Short Answer", "w"],
+    647036320: ["Paragraph", "w"],
+    363426485: ["Multiple Choice", "m", "Option 1", "Option 2"],
+    1142411773: ["Checkboxes", "c", "Option 1", "Option 2"],
+    2116902388: ["Dropdown", "m", "Option 1", "Option 2"],
+    465882654: ["Date", "d"],
+    1049988990: ["Time", "t"],
 }
 
 # Interactive form input
@@ -42,29 +40,22 @@ def form_input(entries):
     for entry, (title, type, *choices) in entries.items():
         # Print title and choices if needed
         print(f" === {title} === ")
-        if type in {"Multiple Choice", "Dropdown", "Checkboxes"}:
-            for i, choice in enumerate(choices, start=1):
-                print(f" [{i}] {choice}")
+        for i, choice in enumerate(choices, start=1):
+            print(f" [{i}] {choice}")
 
         # Different input methods for the types
-        if type == "Short Answer":
-            response = input("Short Answer: (one line) ")
-        elif type == "Paragraph":
-            print("Paragraph: (empty line to end)")
-            lines = []
-            while line := input():
-                lines.append(line)
-            response = "\n".join(lines)
-        elif type in {"Multiple Choice", "Dropdown"}:
-            line = input(f"{type}: (type number in brackets) ")
+        if type == "w":
+            response = input("Text: (one line) ")
+        elif type == "m" :
+            line = input("Choice: (type number in brackets) ")
             response = choices[int(line.strip()) - 1]
-        elif type == "Checkboxes":
+        elif type == "c":
             line = input("Checkboxes: (type number in brackets separated by commas) ")
             indices = [int(part.strip()) - 1 for part in line.split(",")]
             responses = [choices[i] for i in indices]
-        elif type == "Date":
+        elif type == "d":
             response = input("Date: (format as MM/DD/YYYY) ").split("/")
-        elif type == "Time":
+        elif type == "t":
             response = input("Date: (format as HH:MM) ").split(":")
         else:
             raise ValueError(f"Unknown type: {type!r}")
