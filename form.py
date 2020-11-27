1cf85e4321c7c5d67ddd3e1c702cedf2b6e8aad7
George Zhang <geetransit@gmail.com>
2020-11-26 21:26:13 -0500

    Simplify top level error handling

--- form.py
+++ form.py
@@ -586,10 +586,7 @@ if __name__ == "__main__":
 
     try:
         main(args)
-    # Apparently an empty except catches SystemExit. This prevents it.
-    except SystemExit:
-        raise
-    except Exception:
+    except Exception:  # This won't catch Ctrl+C or sys.exit
         traceback.print_exc()
         sys.exit(4)
     finally:
